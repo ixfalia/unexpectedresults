aef279bcb434d75c82179374ba234ebc2ad380be
weylinetra_ <weylinetra@gmail.com>
2018-11-03 16:22:51 -0700

    Levels, Camera, and Gravity
    Starting to populate level
    Started on controling camera
    Removed gravity from level

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    [SerializeField]
+    GameObject targetPlayer;
+
+    [SerializeField]
+    float LeftAndRight_ = 0.0f;
+    [SerializeField]
+    float TopAndBottom_ = 0.0f;
+    [SerializeField]
+    float MinCheck_ = 0.1f;
+	// Use this for initialization
+	void Start () {
+        Vector2 camAdjustment = Vector2.zero;
+        var pos = transform.position;
+        var pPos = targetPlayer.transform.position;
+        if (pPos.x > pos.x + MinCheck_ && pos.x < LeftAndRight_)
+        {
+            camAdjustment.x = 1.0f * (pPos.x - pos.x);
+        }
+        else if(pPos.x < pos.x - MinCheck_ && pos.x > -LeftAndRight_)
+        {
+            camAdjustment.x = -1.0f * (pPos.x - pos.x);
+        }
+        pos.x += camAdjustment.x;
+        transform.position = pos;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
