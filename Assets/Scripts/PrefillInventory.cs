788571e1d2bd677999ed8b857d5c5933f171a6ad
weylinetra_ <weylinetra@gmail.com>
2018-11-04 13:57:50 -0800

    Added type thing for inventory

---
+++ Assets/Scripts/PrefillInventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefillInventory : MonoBehaviour {
+    [SerializeField]
+    string target_ = "";
+    [SerializeField]
+    List<ElementTypes> lists_ = new List<ElementTypes>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            for(int i= 0; i < lists_.Count;++i)
+            {
+                GameObject.Find(target_).GetComponent<Inventory>().list_.Add(lists_[i]);
+            }
+            
+        }
+	}
+}
