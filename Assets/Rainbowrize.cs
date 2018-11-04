7221cfcc6ed77fd65c21fa3e0360d7685c170842
Andrew <ixfalia+github@gmail.com>
2018-11-04 01:51:38 -0700

    more UI scripts for the mindblender

---
+++ Assets/Rainbowrize.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rainbowrize : MonoBehaviour
+{
+    [SerializeField]
+    Color changeColor_ = new Color();
+
+    [SerializeField]
+    float duration_ = 1.0f;
+
+    [SerializeField]
+    bool isRepeat_ = true;
+
+    [SerializeField]
+    bool isRainbow_ = true;
+
+    private float elapsed = 0.0f;
+    private float counter = 0.0f;
+    private float neg = 1.0f;
+    private Color startingColor;
+    private Color targetColor;
+    private Dictionary<string, Color> ColorsDict;
+    private List<Color> ColorList;
+    private IEnumerator colorIterator;
+    private int colorI = 0;
+
+		// Use this for initialization
+	void Start ()
+	{
+        ColorsDict = new Dictionary<string, Color>();
+        ColorList = new List<Color>();
+
+
+        ColorsDict.Add("red", Color.red);
+        ColorsDict.Add("yellow", Color.yellow);
+        ColorsDict.Add("green", Color.green);
+        ColorsDict.Add("blue", Color.blue);
+
+        ColorList.Add(Color.red);
+        ColorList.Add(Color.yellow);
+        ColorList.Add(Color.green);
+        ColorList.Add(Color.blue);
+
+        if (isRainbow_)
+        {
+            targetColor = ColorList[colorI];
+        }
+        else
+            targetColor = changeColor_;
+
+        startingColor = GetComponent<Canvas>().color;
+
+        colorIterator = ColorList.GetEnumerator();
+	}
+	
+		// Update is called once per frame
+	void Update ()
+	{
+        elapsed += Time.deltaTime;
+
+        counter += neg * Time.deltaTime;
+        float t = counter / duration_;
+        var spriteRender = GetComponent<>();
+        Color currentColor = spriteRender.color;
+
+        if (currentColor != null)
+        {
+            Color newColor = Color.Lerp(startingColor, targetColor, t);
+            currentColor = newColor;
+        }
+
+        if( isRainbow_ && counter > duration_ )
+        {
+            ++colorI;
+            colorI %= ColorList.Count;
+
+            targetColor = ColorList[colorI];
+
+            counter = 0.0f;
+        }
+        else if (isRepeat_)
+            checkAndReset(ref counter, ref duration_, ref neg);
+    }
+
+    void checkAndReset(ref float timecounter, ref float duration, ref float neg)
+    {
+        if (timecounter >= duration ^ timecounter <= 0.0f)
+        {
+            //timecounter = 0.0f;
+            neg = -neg;
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
--- Assets/Scripts/Drifter.cs
+++ Assets/Scripts/Drifter.cs
@@ -8,59 +8,69 @@ public class Drifter : MonoBehaviour
     bool isRepeat = true;
 
     [SerializeField]
-    Vector3 drift_ = new Vector3(0, 0, 0);
+    Vector3 drift_ = new Vector3(0, 0, 0), durations_ = new Vector3(1.0f, 1.0f, 1.0f);
 
     [SerializeField]
-    float duration_ = 1.0f, offset_ = 0.0f;
+    float offset_ = 0.0f;
 
     private float elapsed = 0.0f;
     private Vector3 timeCounters = new Vector3(0, 0, 0);
+    private float timeCounter = 0.0f;
     private Vector3 StartingPosition;
-    private float neg = 1.0f;
+    private Vector3 negs = new Vector3(1.0f,1.0f,1.0f);
+    //private float neg = 1.0f;
 
 		// Use this for initialization
 	void Start ()
 	{
-        StartingPosition = GetComponent<RectTransform>().anchoredPosition3D;
+        StartingPosition = GetComponent<RectTransform>().anchoredPosition;
 	}
 	
 		// Update is called once per frame
 	void Update ()
 	{
-        elapsed += Time.deltaTime;
-        float delta = neg * Time.deltaTime;
+        //if (!isRepeat && (elapsed > durations_.x || elapsed > durations_.y || elapsed > durations_.z))
+        //    return;
 
-        timeCounters.x += delta;
-        timeCounters.y += delta;
-        timeCounters.z += delta;
+        elapsed += Time.deltaTime;
+        float delta = Time.deltaTime;
+
+        timeCounters.x += negs.x * delta;
+        timeCounters.y += negs.y * delta;
+        timeCounters.z += negs.z * delta;
 
         if (elapsed >= offset_)
         {
             var componentTransform = GetComponent<RectTransform>();
 
-            if(componentTransform != null)
+            if (componentTransform != null)
             {
-                Vector3 translate = componentTransform.localPosition;
+                Vector3 translate = new Vector3();
 
-                translate.x = Mathf.Lerp(StartingPosition.x, StartingPosition.x + drift_.x, timeCounters.x);
-                translate.y = Mathf.Lerp(StartingPosition.y, StartingPosition.y + drift_.y, timeCounters.y);
-                translate.z = Mathf.Lerp(StartingPosition.z, StartingPosition.z + drift_.z, timeCounters.z);
+                translate.x = Mathf.Lerp(StartingPosition.x, StartingPosition.x + drift_.x, timeCounters.x / durations_.x);
+                translate.y = Mathf.Lerp(StartingPosition.y, StartingPosition.y + drift_.y, timeCounters.y / durations_.y);
+                translate.z = Mathf.Lerp(StartingPosition.z, StartingPosition.z + drift_.z, timeCounters.z / durations_.z);
 
-                componentTransform.localPosition = translate;
+                componentTransform.anchoredPosition = translate;
             }//end if
 
-            checkAndReset(ref timeCounters.x, ref duration_);
-            checkAndReset(ref timeCounters.y, ref duration_);
-            checkAndReset(ref timeCounters.z, ref duration_);
+            checkAndReset(ref timeCounters.x, ref durations_.x, ref negs.x);
+            checkAndReset(ref timeCounters.y, ref durations_.y, ref negs.y);
+            checkAndReset(ref timeCounters.z, ref durations_.z, ref negs.z);
         }//end if
+        else
+        {
+            int flower = 0;
+        }
 	}//end Update()
 
-    void checkAndReset(ref float timecounter, ref float duration)
+    void checkAndReset(ref float timecounter, ref float duration, ref float neg)
     {
-        if(timecounter >= duration || timecounter <= 0.0f)
+        if(timecounter >= duration ^ timecounter <= 0.0f)
         {
             //timecounter = 0.0f;
             neg = -neg;
+            return;
         }
         else
         {
