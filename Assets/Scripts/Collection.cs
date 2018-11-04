c5b2117877adf7b7740d107fb0ed361cabd73a74
weylinetra_ <weylinetra@gmail.com>
2018-11-04 01:58:17 -0800

    Two more levels
    Some minor updates to scripts so they work
    added two levels

--- Assets/Scripts/Collection.cs
+++ Assets/Scripts/Collection.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Collection : MonoBehaviour {
     [System.NonSerialized]
     public ElementTypes TempTypes = null;
+    [System.NonSerialized]
+    public Teleporter TempPorter = null;
     bool FirstCheck = true;
 	// Use this for initialization
 	void Start () {
@@ -13,24 +15,28 @@ public class Collection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetAxis("Fire2") > 0.1 && FirstCheck && GetComponent<CollisionChecks>().TouchingElementalInteractable_ == true)
+        if (Input.GetAxis("Fire2") > 0.1 && FirstCheck && GetComponent<CollisionChecks>().TouchingElementalInteractable_ == true)
         {
             var inventory = GetComponent<Inventory>();
             bool isGoodToAdd = true;
-            for(int i = 0; i < inventory.list_.Count; ++i)
+            for (int i = 0; i < inventory.list_.Count; ++i)
             {
-                if(TempTypes.name_ == inventory.list_[i].name_)
+                if (TempTypes.name_ == inventory.list_[i].name_)
                 {
                     isGoodToAdd = false;
                 }
             }
-            if(isGoodToAdd)
+            if (isGoodToAdd)
             {
                 inventory.list_.Add(TempTypes);
             }
             FirstCheck = false;
         }
-        else if(Input.GetAxis("Fire2") < 0.1)
+        else if (Input.GetAxis("Fire2") > 0.1 && FirstCheck && GetComponent<CollisionChecks>().TouchingTeleporterInteractable_ == true)
+        {
+            TempPorter.Teleport();
+        }
+        else if (Input.GetAxis("Fire2") < 0.1)
         {
             FirstCheck = true;
         }
